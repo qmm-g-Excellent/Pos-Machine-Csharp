ea14930983afebff6e91ab11274a336bdb3da446
qmm-g-Excellent <qmmahbc@163.com>
2017-08-25 00:38:29 +0800

    refactor code with api

--- src/pos.Machine/PosMachine.cs
+++ src/pos.Machine/PosMachine.cs
@@ -1,61 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Autofac;
 
 namespace pos.Machine
 {
     public interface IPosMachine
     {
-         string GetReceipt(string[] items);
+        string GetReceipt(string[] items);
     }
 
     internal class PosMachine : IPosMachine
     {
         public string GetReceipt(string[] items)
         {
-            if (items.Length < 1)
-            {
-                throw new ArgumentNullException();
-    
-            }
+            this.WillThrowsException(items);
             Dictionary<string, int> dict = new Dictionary<string, int>();
+            this.GetDictionary(dict, items);
+            long total = 0;
+            return  dict.Aggregate(
+                                 "*** Receipt ***\n",
+                (subReceipt, item) =>
+                {
+                    var productInformation = new ProductRepository().Get(item.Key.ToString());
+                    long subTotal = item.Value * productInformation.Price;
+                    total += subTotal;
+                    return subReceipt += "Name: " + productInformation.Name + ", Amount: " + item.Value + ", Price: " +
+                                         productInformation.Price + ", Total: " + subTotal + "\n";
+                })+ " ---------------\n" +
+                " Total: " + total;
+//            return "*** Receipt ***\n" + receipt +
+//                   " ---------------\n" +
+//                   " Total: " + total;
+        }
+
+        public void GetDictionary(Dictionary<string, int> dict , string[] items)
+        {
             foreach (string item in items)
             {
-                if (item.Length < 10)
+                if (!dict.ContainsKey(item))
+                    dict.Add(item, 1);
+                else
+                    dict[item] += 1;
+            }
+        }
+
+        public void WillThrowsException(string[] items)
+        {
+            if (items.Length < 1)
+                throw new ArgumentNullException();
+            else
+            {
+                string[] incorrectFormat = items.Where(ele => ele.Length < 10).ToArray();
+                if (incorrectFormat.Length > 0)
                 {
                     throw new FormatException();
                 }
-                int count = this.getRepeatItem(item, items);
-                if (!dict.ContainsKey(item))
-                {
-                    dict.Add(item, count);
-                }
             }
-
-            string receipt = "";
-            long total = 0;
-            foreach (KeyValuePair<string, int> item in dict)
-            {
-                var productInformation = new ProductRepository().Get(item.Key.ToString());
-                long subTotal = item.Value * productInformation.Price;
-                total += subTotal;
-                receipt += "Name: " + productInformation.Name + ", Amount: " + item.Value + ", Price: " +
-                           productInformation.Price + ", Total: " + subTotal + "\n";
-            }
-            return "*** Receipt ***\n" + receipt +
-                   " ---------------\n" +
-                   " Total: " + total;
-        }
-
-        public int getRepeatItem(string barcode, string[] items)
-        {
-            int count = 0;
-            foreach (string item in items)
-            {
-                if (barcode == item)
-                     count++;
-            }
-            return count;
         }
     }
 }
